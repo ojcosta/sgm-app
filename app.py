07f90ef2d7a542cec78d4dc8019ef03292e5a3d2
Costa, J. <jonassilvaalvesdacosta@outlook.com>
2026-04-09 06:05:22 -0300

    Atualizar o app.py

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ LISTA_MARCA = [
 def autenticacao():
     USUARIOS_PERMITIDOS = {
         "jonascosta": "MENGo2026@", "rebecaalves": "33091221", "wilsonalves": "RR2026", 
-        "oficina": "oficina123", "pedrobueno": "oficina1234"
+        "oficina": "1234", "pedrobueno": "oficina1234"
     }
     
     if "logado" not in st.session_state:
