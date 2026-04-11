50b8b91fd9111c3c952732fa5885dc651b6f64c9
Costa, J. <jonassilvaalvesdacosta@outlook.com>
2026-04-10 21:03:56 -0300

    Update app.py

--- app.py
+++ app.py
@@ -89,7 +89,10 @@ if autenticacao():
         if "lista_servicos_temp" not in st.session_state:
             st.session_state.lista_servicos_temp = []
             try:
-                st.dataframe(df_temp[['SERVIÇO', 'CUSTO (RS)', DIAGNÓSTICO]], use_container_width=True)
+                ultima_os = pd.to_numeric(df['OS'], errors='coerce').max()
+                if pd.isna(ultima_os):
+                    st.session_state.proxima_os = 1
+                else st.session_state.proxima_os = int(ultima_os) + 1
             except:
                 st.session_state.proxima_os = len(df) + 1
             
