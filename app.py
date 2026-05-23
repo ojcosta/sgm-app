7bc4e1dd28c836247414e88041a2cd89dd9f56f4
Costa, J. <jonassilvaalvesdacosta@outlook.com>
2026-05-23 19:29:51 -0300

    Update app.py

--- app.py
+++ app.py
@@ -73,7 +73,7 @@ def autenticacao():
             
             c1, c2, c3 = st.columns(3)
             c1.metric("STATUS", "Online")
-            c2.metric("VERSÃO", "0.9.3b")  # Versão atualizada
+            c2.metric("VERSÃO", "0.9.2b")  # Versão atualizada
             c3.metric("SUPORTE", "Ativo")
             
         return False
